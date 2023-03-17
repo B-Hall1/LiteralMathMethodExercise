be1351b166e890c801b46fdcfab2ff70556c27ae
Blair Hall <blair.hall.og@gmail.com>
2023-03-16 22:59:12 -0400

    Literal Math Method Exercise

---
+++ script.js
@@ -0,0 +1,9 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase())
+var beenImpaled = "Oh, look at that. I've been impaled.";
+var dotDotDot = "..."
+var skullBones = "I don't have a skull" + dotDotDot + "or bones.";
+console.log(skullBones);
+console.log(Math.PI);
+var randomNumber = Math.floor(Math.random()*3+1);
+console.log(randomNumber);
